bc48083ecf91fe86daf4619d536984fb82355bd2
prasannashetty <pgshetty2006@gmail.com>
2018-05-17 15:48:01 +0530

    Added debug lines

--- AutomationScripts/kubenix/podsStatus.sh
+++ AutomationScripts/kubenix/podsStatus.sh
@@ -3,9 +3,11 @@
 beforeJoin=$(kubectl get po --all-namespaces | wc -l)
 beforeJoin=$(($beforeJoin-1))
 afterJoin=0
+echo "While loop started"
 while(("$beforeJoin" != "$afterJoin"))
 do
 	OUTPUT=$(kubectl get po --all-namespaces | grep -i "Running" | wc -l)
 	afterJoin=$OUTPUT
 	sleep 30
 done
+echo "While loop Ended"
