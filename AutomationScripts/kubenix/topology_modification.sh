19f606fe0904eda74ba91ac7c6e6d31f3546c60e
prasannashetty <pgshetty2006@gmail.com>
2018-05-21 06:38:48 +0530

    handle disktype as input parameter

--- AutomationScripts/kubenix/topology_modification.sh
+++ AutomationScripts/kubenix/topology_modification.sh
@@ -1,21 +1,26 @@
 list_gfs_ip_address=( $(echo $1|tr "," "\n") )
+
 template_password=$2
 template_username=root
-echo "if root user password is not changeme, topology_modification.sh will fail to add manage hostname to topology.json"
-#sudo su
+
+disk_type=$3
+path_var1=$(echo $disk_type | cut -f2 -d/)
+path_var2=$(echo $disk_type | cut -f3 -d/)
+
 cp /root/topology.json.sample /root/topology.json
 i=0
 for ip in ${list_gfs_ip_address[@]}
 do
+        echo "loop $i" 
         hostName=$(sshpass -p ${template_password} ssh -o StrictHostKeyChecking=no ${template_username}@${ip} sudo hostname)
+        echo $hostName
         jq ".clusters[0].nodes[$i].node.hostnames.manage = ([\"${hostName}\"])" < /root/topology.json > /root/topology1.json
         mv /root/topology1.json /root/topology.json
         jq ".clusters[0].nodes[$i].node.hostnames.storage = ([\"${ip}\"])" < /root/topology.json > /root/topology1.json
         mv /root/topology1.json /root/topology.json
         jq ".clusters[0].nodes[$i].node.zone = 1" < /root/topology.json > /root/topology1.json
         mv /root/topology1.json /root/topology.json
-        jq ".clusters[0].nodes[$i].devices = ([\"\/dev\/xvda\"])" < /root/topology.json > /root/topology1.json
+        jq ".clusters[0].nodes[$i].devices = ([\"/$path_var1/$path_var2\"])" < /root/topology.json > /root/topology1.json
         mv /root/topology1.json /root/topology.json
         i=$(($i+1))
-        #IPHOSTNAMEMAP[$ip]=$hostName
 done
