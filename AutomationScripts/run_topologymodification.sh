b1a4cad0d741868de7e7a1e665db11a4256e24ce
prasannashetty <pgshetty2006@gmail.com>
2018-05-21 06:40:42 +0530

    included disk_type parameter

--- AutomationScripts/run_topologymodification.sh
+++ AutomationScripts/run_topologymodification.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 password=$1
+disk_type=$2
 sed -i 's/\[//g'  /root/node_addresses.txt
 sed -i 's/\]//g' /root/node_addresses.txt
 glusterfs_ips=`cat /root/node_addresses.txt`
 
-/root/topology_modification.sh $glusterfs_ips $password >> /root/k8sinstallation_status.log
+/root/topology_modification.sh $glusterfs_ips $password $disk_type >> /root/k8sinstallation_status.log
