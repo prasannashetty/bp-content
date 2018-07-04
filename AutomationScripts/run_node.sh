903c9723ab25ded0569d84817a358358d0429f45
prasannashetty <pgshetty2006@gmail.com>
2018-07-04 15:59:40 +0530

    Added /etc/hosts changes

--- AutomationScripts/run_node.sh
+++ AutomationScripts/run_node.sh
@@ -4,6 +4,7 @@ sed -i 's/\[//g'  /root/node_addresses.txt
 sed -i 's/\]//g' /root/node_addresses.txt
 value=`cat /root/node_addresses.txt`
 arr=$(echo $value | tr "," "\n")
+count=1
 
 for node in $arr
 do
@@ -11,4 +12,7 @@ do
     sshpass -p $password  scp  -o "StrictHostKeyChecking no" /tmp/join_node.sh root@$node:/tmp/
     sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'chmod 700 /tmp/join_node.sh'
     sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node '/tmp/join_node.sh'
+    sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'echo $node node${count}.kubernetes.cluster >> /etc/hosts'
+    sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'sudo hostname node${count}.kubernetes.cluster'
+    $count++
 done
