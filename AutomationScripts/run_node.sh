9ba1db3f593d17f9beff5fa2c068bf0664bc4ab2
prasannashetty <pgshetty2006@gmail.com>
2018-07-04 19:04:05 +0530

    Added logic to increment node counts.

--- AutomationScripts/run_node.sh
+++ AutomationScripts/run_node.sh
@@ -12,7 +12,13 @@ do
     sshpass -p $password  scp  -o "StrictHostKeyChecking no" /tmp/join_node.sh root@$node:/tmp/
     sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'chmod 700 /tmp/join_node.sh'
     sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node '/tmp/join_node.sh'
-    sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'echo $node node${count}.kubernetes.cluster >> /etc/hosts'
-    sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'sudo hostname node${count}.kubernetes.cluster'
-    $count++
+
+    if [ $count = 1 ]
+    then
+    	sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'echo 1 >> /root/count.txt'
+    else
+    	sshpass -p $password  scp  -o "StrictHostKeyChecking no" root@$previous_node:/root/count.txt root@node:/root/count.txt
+	    sshpass -p $password  ssh  -o "StrictHostKeyChecking no" root@$node 'count=$(</root/count.txt);echo "$(ifconfig $(route | grep '^default' | grep -o '[^ ]*$') | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1) node$count.kubernetes.cluster" >> /etc/hosts;echo $((count+1)) > /root/count.txt;sudo hostname node$count.kubernetes.cluster'
+	fi
+	previous_node = $node
 done
